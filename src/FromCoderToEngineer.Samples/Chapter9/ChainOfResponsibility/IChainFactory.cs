6f42d992f7862aa34ce159306faab38530bc21f4
Michael Bradvica <mjbradvica@protonmail.com>
2021-03-07 21:01:51 -0800

    added chain of responsibility base classes

---
+++ src/FromCoderToEngineer.Samples/Chapter9/ChainOfResponsibility/IChainFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace FromCoderToEngineer.Samples.Chapter9.ChainOfResponsibility
+{
+    public interface IChainFactory<T>
+    {
+        IChainHandler<T> CreateChain();
+    }
+}
---
+++ src/FromCoderToEngineer.Samples/Chapter9/ChainOfResponsibility/IChainHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace FromCoderToEngineer.Samples.Chapter9.ChainOfResponsibility
+{
+    using System.Threading.Tasks;
+
+    public interface IChainHandler<T>
+    {
+        Task<T> Handle(T command);
+    }
+}
