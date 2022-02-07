82bcf14bb5b4924b3c2c5b9b67bf2187ed302fa8
Michael Bradvica <mjbradvica@protonmail.com>
2022-02-07 16:53:55 -0500

    update ch1

---
+++ src/FromCoderToEngineer.Samples/Chapter1/NakedPropertiesAndCollections/CustomerWithComplexName.cs
@@ -0,0 +1,34 @@
+﻿using FromCoderToEngineer.Samples.Chapter1.PublicSetters;
+
+namespace FromCoderToEngineer.Samples.Chapter1.NakedPropertiesAndCollections
+{
+    public class CustomerWithComplexName
+    {
+        public CustomerWithComplexName(string firstName, string lastName, string middleName, string preferredName, Honorifics honorifics, Suffix suffix)
+        {
+            Validator.ValidNameFormat(firstName);
+            Validator.ValidNameFormat(lastName);
+            Validator.ValidNameFormat(middleName);
+            Validator.ValidNameFormat(preferredName);
+
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+            PreferredName = preferredName;
+            Honorifics = honorifics;
+            Suffix = suffix;
+        }
+
+        public string FirstName { get; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; }
+
+        public string PreferredName { get; set; }
+
+        public Honorifics Honorifics { get; set; }
+
+        public Suffix Suffix { get; set; }
+    }
+}
--- src/FromCoderToEngineer.Samples/Chapter1/PrivateMethods/CustomerWithPrivateValues.cs
+++ src/FromCoderToEngineer.Samples/Chapter1/PrivateMethods/CustomerWithPrivateValues.cs
@@ -4,11 +4,15 @@ namespace FromCoderToEngineer.Samples.Chapter1.PrivateMethods
 {
     public class CustomerWithPrivateValues
     {
-        // Other properties and methods omitted for brevity.
-
         private readonly DateTime _availableMinimum;
 
         private readonly DateTime _availableMaximum;
+
+        public CustomerWithPrivateValues(DateTime minAvailability, DateTime maxAvailability)
+        {
+            _availableMinimum = minAvailability;
+            _availableMaximum = maxAvailability;
+        }
 
         public bool AvailabilityMatchesCustomer(DateTime reservationAvailability)
         {
