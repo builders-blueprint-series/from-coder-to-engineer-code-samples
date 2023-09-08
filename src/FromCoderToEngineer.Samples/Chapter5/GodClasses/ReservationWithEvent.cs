69cb75ec92e4091308a4b8d1ef75e27c17c21074
Michael B <mjbradvica@gmail.com>
2023-09-08 17:58:35 -0400

    Create ReservationWithEvent.cs

---
+++ src/FromCoderToEngineer.Samples/Chapter5/GodClasses/ReservationWithEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FromCoderToEngineer.Samples.Chapter5.GodClasses
+{
+    public class ReservationWithEvent : Entity
+    {
+        public Guid Id { get; set; }
+
+        public ReservationWithEvent()
+        {
+            AddNotification(new ReservationCreated());
+        }
+    }
+}
